64c788bdf30473bcb0c5a0d99f752bf4ecec4194
Jesus <jjcorrales76@gmail.com>
2025-09-10 10:35:25 -0600

    added types to die comp

--- src/Die.tsx
+++ src/Die.tsx
@@ -1,17 +1,26 @@
+import type { CSSProperties, JSX, MouseEventHandler } from "react";
 
+type DieProps = {
+    hold: MouseEventHandler
+    isHeld: boolean
+    value:string
 
-export default function Die(props) {
-    const styles = {
-        backgroundColor: props.isHeld ? "#59E391" : "white"
-    }
-    
-    return (
-        <button 
-            style={styles}
-            onClick={props.hold}
-            aria-pressed={props.isHeld}
-            aria-label={`Die with value ${props.value}, 
+}
+
+export default function Die(props: DieProps): JSX.Element {
+  const styles: CSSProperties = {
+    backgroundColor: props.isHeld ? "#59E391" : "white",
+  };
+
+  return (
+    <button
+      style={styles}
+      onClick={props.hold}
+      aria-pressed={props.isHeld}
+      aria-label={`Die with value ${props.value}, 
             ${props.isHeld ? "held" : "not held"}`}
-        >{props.value}</button>
-    )
+    >
+      {props.value}
+    </button>
+  );
 }
